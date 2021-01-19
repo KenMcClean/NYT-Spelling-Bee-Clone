074ae9d1bc3455426adc6b1bef86b501f9d3fbed
KenMcClean <65670906+KenMcClean@users.noreply.github.com>
2021-01-19 15:18:10 -0800

    Update NYTBeeClone.ps1

--- NYTBeeClone.ps1
+++ NYTBeeClone.ps1
@@ -16,31 +16,26 @@ $attempt = read-host "Enter a word: "
 $attempt = $attempt.ToUpper()
 
 
+if($attempt.Contains($anagramKey) ){
+    if($attempt.Length -gt 3){
+        if($foundWordsList -notcontains $attempt){
+        if($wordlist -contains $attempt){
+                    write-host "valid word"
+                    $score += $attempt.Length}
+                    
+elseif($wordlist -notcontains $attempt)
+    {write-host "Invalid word"}}
+                    
+elseif($foundWordsList -contains $attempt){
+    write-host "Word already found"}}
 
-
-if($attempt.Contains($anagramKey) )
-{
-
-if ($attempt.Length -gt 3)
-{
-
-if($foundWordsList -notcontains $attempt){
-
-write-host "valid word"
-$score += $attempt.Length
-
-}elseif($foundWordsList -contains $attempt){
-write-host "Word already found"
-
-}
-
-}
 elseif($attempt.length -lt 4){
-write-host "Word must be longer than three letters"
-}
-}elseif($attempt -notcontains ($anagramKey)){
-write-host "Word must contain key"
-}
+        write-host "Word must be longer than three letters"}}
+
+ elseif($attempt -notcontains ($anagramKey)){
+         write-host "Word must contain key"}
+
+
 write-host "Words found: "$foundWordsList
 write-host "Score: "$score
 
